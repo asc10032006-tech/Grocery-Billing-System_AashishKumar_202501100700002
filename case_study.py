c66aa99546f87bfe5357659e1e995461d22372ba
asc10032006-tech <asc10032006@gmail.com>
2026-02-19 16:32:46 +0530

    Fix formatting

--- case_study.py
+++ case_study.py
@@ -1,7 +1,7 @@
-item1=float(input("Enter the price of item 1:"))
-item2=float(input("Enter the price of item 2:"))
-item3=float(input("Enter the price of item 3:"))
-total_cost=item1+item2+item3
+item1 = float(input("Enter the price of item 1:"))
+item2 = float(input("Enter the price of item 2:"))
+item3 = float(input("Enter the price of item 3:"))
+total_cost = item1+item2+item3
 discount = 0
 if total_cost > 50:
     discount = 0.10 * total_cost
